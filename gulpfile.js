68fecb8afef60c1294f5ced3f5c44d3e8696bc01
n4nirmal09 <n4nirmal09@gmail.com>
2018-02-24 23:29:11 +0530

    first addition

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const gulp      = require("gulp");
+const gutil     = require("gulp-util");
+//const uglify    = require("gulp-uglifyjs");
+const plumber   = require('gulp-plumber');
+const notify    = require("gulp-notify");
+const webserver = require('gulp-webserver');
+const compass   = require("gulp-compass");
+const webpack   = require('webpack');
+const webpackStream = require('webpack-stream');
+const webpackConf = require('./webpack.config.js');
+
+// Gulp Webserver
+gulp.task('webserver', () => {
+  gulp.src('dev')
+    .pipe(notify("Starting Server"))
+    .pipe(webserver({
+      port : 8080,
+      livereload: true,
+      directoryListing: false,
+      open: true,
+      // path : '/dev'
+    }));
+});
+
+// Gulp Compass
+gulp.task('compass', () => {
+  gulp.src('./src/sass/**/*.scss')
+    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+    .pipe(compass({
+      config_file: 'config.rb',
+      css: './dist/css',
+      sass:'./src/sass'
+    }))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+// Webpack task 
+gulp.task('webpack', () => {
+	gulp.src(webpackConf.entry)
+	.pipe(webpackStream(webpackConf), webpack)
+	.pipe(gulp.dest(webpackConf.output.publicPath))
+});
